71ee456ee70defa9a8459237910971e0536ce1f5
hailv <zefredzocohen@gmail.com>
2017-02-01 07:20:14 +0700

    dfdfds

--- bk/wp-content/themes/sahifa/js/postviews-cache.js
+++
@@ -1,1 +0,0 @@
-jQuery.ajax({type:"GET",url:tieViewsCacheL10n.admin_ajax_url,data:"postviews_id="+tieViewsCacheL10n.post_id+"&action=postviews",cache:!1});
--- bk/wp-content/themes/sahifa/sidebar-shop.php
+++
@@ -1,7 +0,0 @@
-<aside id="sidebar">
-	<div class="theiaStickySidebar">
-<?php
-	dynamic_sidebar( 'shop-widget-area' );
-?>
-	</div><!-- .theiaStickySidebar /-->
-</aside><!-- #sidebar /-->
